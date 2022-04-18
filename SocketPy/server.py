2f41b88f98825a25fe8ca09a774456916aab1a19
Bryan Saigua <49696462+BryanSaigua@users.noreply.github.com>
2022-04-18 18:03:03 -0500

    socketpy

---
+++ SocketPy/server.py
@@ -0,0 +1,76 @@
+import socket
+import threading
+import sys
+import pickle
+
+from time import sleep
+
+class Server():
+    def __init__(self, host="192.168.43.134", port=5000):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((str(host), int(port)))
+        self.sock.listen(10)
+        self.sock.setblocking(False)
+
+        self.clients = []
+        accept = threading.Thread(target=self.aceptarCon)
+        process = threading.Thread(target=self.processarCon)
+
+        accept.daemon = True
+        accept.start()
+
+        process.daemon = True
+        process.start()
+
+        while True:
+            msg = input('')
+            if msg == 'salir':
+                self.sock.close()   
+                sys.exit()
+            else:
+                pass
+
+    def aceptarCon(self):
+        print("AceptarCon iniciado")
+        while True:
+            try:
+                conn, addr = self.sock.accept()
+                conn.setblocking(False)
+                self.clients.append({
+                    'client': conn,
+                    'data': {}
+                })
+                print("Se conecto un cliente: ", addr)
+            except:
+                pass
+
+    def processarCon(self):
+        print("ProcessarCon iniciado")
+        while True:
+            if len(self.clients) > 0:
+                for c in self.clients:
+                    try:
+                        data = c['client'].recv(1024)
+                        if (data.decode("UTF-8") == 'enviar'):
+                            self.activated = True
+
+                        if (self.activated):
+                            
+                            #print('Enviando...')
+                            msg = "Desde el servidor".encode("UTF-8")
+                            c['client'].send((len(msg).to_bytes(2, byteorder='big')))
+                            c['client'].send((msg))
+                    except: 
+                        pass
+    
+    #def send(self, senal):
+        # enviar el cliente la senal
+
+Server()
+#server = Server()
+
+#while(reading singnals):
+#    if (read sig):
+#        server.send('signhafasd')
+
+
